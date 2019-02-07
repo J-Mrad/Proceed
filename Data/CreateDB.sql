a757d094d1dbd18a86b13da5b39a49796f35defa
Jad Mrad <40995053+J-Mrad@users.noreply.github.com>
2019-02-07 20:58:32 +0200

    Bug fixes

--- Data/CreateDB.sql
+++ Data/CreateDB.sql
@@ -170,7 +170,7 @@ go
 /*==============================================================*/
 create table LOG (
    LOGID                int   IDENTITY(1,1)  not null,
-   EMP                  int                  null,
+   EMP                  varchar(512)         null,
    TYPE                 varchar(32)          null,
    DETAILS              varchar(256)         null,
    DATE                 datetime             null,
@@ -264,11 +264,6 @@ go
 alter table LOAN
    add constraint FK_LOAN_TAKES_CLIENT foreign key (CID)
       references CLIENT (CID)
-go
-
-alter table LOG
-   add constraint FK_LOG_REFERENCE_EMPLOYEE foreign key (EMP)
-      references EMPLOYEE (EID)
 go
 
 alter table RETIREMENTACCOUNT
--- Data/DataSet001.sql
+++ Data/DataSet001.sql
@@ -1,9 +1,3 @@
-/*	Default Employee	*/
-
-INSERT INTO EMPLOYEE(POSID,NAME,PHONE,EMAIL,DOB,SHIFTDURATION,DATEHIRED)
-VALUES(1,'None','00000','employee@removed',null,0,0-0-0000)
-
-
 /*	Positions		*/
 
 INSERT INTO POSITION(POSNAME,SALARY,BONUS,BONUSCYCLEDAYS)
@@ -11,8 +5,14 @@ VALUES('Fired',0,0,0),
 	('Intern',0,100,30),
 	('Part-Time',800,200,90),
 	('Full-Time',1200,400,90)
+GO
+/*	Default Employee	*/
+
+INSERT INTO EMPLOYEE(POSID,NAME,PHONE,EMAIL,DOB,SHIFTDURATION,DATEHIRED)
+VALUES(1,'None','00000','employee@removed',null,0,0-0-0000)
 
 
+GO
 /* 	Account Levels		*/
 
 
@@ -25,6 +25,7 @@ VALUES(0,0,100,200,1,2,2),
 	(5,10000,1600,1000,3,6,4),
 	(6,100000,3200,1000,4,8,5)
 
+GO
 /*	Some Clients		*/
 
 
